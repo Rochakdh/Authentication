0ea7378438a6d628de38d060447803b62f6cd21a
Rochak Dhakal <rochakdh@gmail.com>
2021-06-22 23:53:23 +0545

    Created : userauth app setings.py : pointed to auth_user_model Added : Custom User Model in models.py

---
+++ hospital/userauth/models.py
@@ -0,0 +1,10 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class customUseraDetails(AbstractUser):
+    """Custom User Model where id and contactNumber is included"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    contactNumber = models.CharField(max_length=10)
