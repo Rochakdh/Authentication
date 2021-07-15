33c306f0c96b211a3880fbdcc5029d988b2e47b9
Rochakdh <rochakdh@gmail.com>
2021-07-15 23:12:58 +0545

    changed file names

--- authentication/authentication/wsgi.py
+++ authentication/authentication/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hospital project.
+WSGI config for authentication project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authentication.settings')
 
 application = get_wsgi_application()
